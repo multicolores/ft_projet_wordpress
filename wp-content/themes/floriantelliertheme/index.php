aec2d1c0a71315059a807d6dbb6e488a8815c089
multicolores <florian.tellier02@gmail.com>
2023-02-10 12:08:08 +0100

    ajoue de style de base

--- wp-content/themes/floriantelliertheme/index.php
+++ wp-content/themes/floriantelliertheme/index.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <?php wp_head(); ?>
 </head>
 
-    <body <?php body_class(); ?>>
+<body <?php body_class(); ?>>
 
+    <header>
         <h1>Le projet wordpress de Florian TELLIER</h1>
+    </header>
 
+    <div class="ft_main_content_container">
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
                 <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
@@ -16,12 +20,13 @@
         <?php else : ?>
             <p>There are no posts !</p>
         <?php endif; ?>
+    </div>
 
-        <footer>
-            <p>Projet wordpress de Florian TELLIER</p>
-        </footer>
-        <?php wp_footer(); ?>
+    <footer>
+        <p>Projet wordpress de Florian TELLIER</p>
+    </footer>
+    <?php wp_footer(); ?>
 
-    </body>
-    
+</body>
+
 </html>
